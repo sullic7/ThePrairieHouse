eeb807a514280b621aafb35af186d10449345b4a
sullic7 <sullic7@rpi.edu>
2015-11-14 15:16:39 -0500

    added home and menu bar json files

---
+++ Resources/indexJson.js
@@ -0,0 +1,15 @@
+{
+  "announcements": [
+    {
+      "title": "Cottage Update",
+      "date": "Mar 1, 2015",
+      "text": "We have new neighbors at the Cottage! Larry renovated the original farm house by the barn in the early 1990’s and are thrilled that the cottage will be appreciated and in capable hands."
+    }
+  ],
+  "specialOffers": [
+
+  ],
+  "upcomingEvents": [
+
+  ]
+}
---
+++ Resources/menuJson.js
@@ -0,0 +1,40 @@
+{
+  "menuBar": [
+    {
+      "menuName": "Home",
+      "menuURL": ""
+    },
+    {
+      "menuName": "The House",
+      "menuURL": ""
+    },
+    {
+      "menuName": "Gallery",
+      "menuURL": ""
+    },
+    {
+      "menuName": "The Area",
+      "menuURL": ""
+    },
+    {
+      "menuName": "Booking",
+      "menuURL": ""
+    },
+    {
+      "menuName": "Events",
+      "menuURL": ""
+    },
+    {
+      "menuName": "Wellness Services",
+      "menuURL": ""
+    }
+    {
+      "menuName": "Reviews",
+      "menuURL": ""
+    }
+    {
+      "menuName": "Directions",
+      "menuURL": ""
+    }
+  ]
+}
