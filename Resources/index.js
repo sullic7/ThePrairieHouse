ee97e772035c9d861cfd861632fb6e7ffbf10e8f
sullic7 <sullic7@rpi.edu>
2015-11-15 00:05:38 -0500

    more formatting

--- Resources/index.js
+++ Resources/index.js
@@ -1,30 +1,30 @@
 function getJS(fileName) {
   return $.getJSON(fileName, function(data) { 
-    var output = '<h2>' + data.announcements.header + '</h2><ul>';
+    var output = '<div><h2>' + data.announcements.header + '</h2><ul>';
       $.each(data.announcements.content, function(i, item) {
         output += '<li class="title">' + item.title + '</li>';
         output += '<li class="date">' + item.date + '</li>';
-        output += '<li class="text">' + item.text + '</li>';
+        output += '<li class="text">' + item.text + '</li></div>';
       });
       output += '</ul>';
       self.focus();
       $('#announcements').html(output);
 
-      var output2 = '<h2>' + data.specialOffers.header + '</h2><ul>';
+      var output2 = '<div><h2>' + data.specialOffers.header + '</h2><ul>';
       $.each(data.specialOffers.content, function(i, item) {
         output2 += '<li class="title">' + item.title + '</li>';
         output2 += '<li class="date">' + item.date + '</li>';
-        output2 += '<li class="text">' + item.text + '</li>';
+        output2 += '<li class="text">' + item.text + '</li></div>';
       });
       output2 += '</ul>';
       self.focus();
       $('#specialOffers').html(output2);
 
-      var output3 = '<h2>' + data.upcomingEvents.header + '</h2><ul>';
+      var output3 = '<div><h2>' + data.upcomingEvents.header + '</h2><ul>';
       $.each(data.upcomingEvents.content, function(i, item) {
         output3 += '<li class="title">' + item.title + '</li>';
         output3 += '<li class="date">' + item.date + '</li>';
-        output3 += '<li class="text">' + item.text + '</li>';
+        output3 += '<li class="text">' + item.text + '</li></div>';
       });
       output3 += '</ul>';
       self.focus();
