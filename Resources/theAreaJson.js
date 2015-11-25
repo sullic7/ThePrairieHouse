246e24ad83574aa5e045ebbc728afb58de422476
sullic7 <sullic7@rpi.edu>
2015-11-25 16:53:21 -0500

    added content to the House and Area pages, started JSON files for those pages

---
+++ Resources/theAreaJson.js
@@ -0,0 +1,82 @@
+{
+	"items": [
+		{
+			"section": "On-Site",
+			"sectionItems": [
+				{
+					"sectionTitle": "Boating",
+					"description": ""
+				},
+				{
+
+				}
+			]
+		},
+		{
+			"secton": "Local Favorites",
+			"sectionItems": [
+				{
+
+				},
+				{
+
+				}
+			]
+		},
+		{
+			"section": "Online Visitor Guides",
+			"sectionItems": [
+				{
+
+				},
+				{
+
+				}
+			]
+		},
+		{
+			"section": "Chamber of Commerce Links",
+			"sectionItems": [
+				{
+
+				},
+				{
+
+				}
+			]
+		},
+		{
+			"section": "Local Favorites by Village",
+			"sectionItems": [
+				{
+
+				},
+				{
+
+				}
+			]
+		},
+		{
+			"section": "Local Artisanry",
+			"sectionItems": [
+				{
+
+				},
+				{
+
+				}
+			]
+		},
+		{
+			"section": "Event Resources",
+			"sectionItems": [
+				{
+
+				},
+				{
+
+				}
+			]
+		}
+	]
+}
---
+++ Resources/theHouseJson.js
@@ -0,0 +1,36 @@
+{
+  "features":
+    {
+      "header": "Features",
+      "content": [
+        {
+          "title": "Upstairs",
+          "text": [ 
+          ]
+        },
+        {
+          "title": "Downstairs",
+          "text": [
+          ]
+        }
+      ]
+    },
+  "amenities":
+    {
+      "header": "Amenities",
+      "content": [ "2-car garage",
+                  "Wifi",
+                  "Basic extended cable TV/DVD/CD",
+                  "Binoculars, spotting scope, and Peterson nature guides",
+                  "Well-stocked kitchen equipped to serve gourmet meals for 12",
+                  "Local Collectiva coffee and tea selection",
+                  "Charcoal Weber grill (with charcoal/electric starter/grill tools)",
+                  "Firewood for the fireplace and fire pit",
+                  "Linens/towels and laundry room",
+                  "Hotel toiletries",
+                  "Variety of indoor/outdoor games including bocce ball, bean bag toss, and volleyball equipment for the spacious backyard",
+                  "2 kayaks and 2 canoes with life jackets",
+                  "Guests also enjoy hiking the trails traversing the 120 acre property"
+      ]
+    }
+}
